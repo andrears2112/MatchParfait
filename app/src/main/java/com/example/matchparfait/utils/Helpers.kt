d36413b95087117b82527dbe7d812e6ccc2a4947
Andrea Rios <arios@fc5.mx>
2024-05-27 01:30:48 -0700

    Feature: funciones para el manejo de direccion de envio

--- app/src/main/java/com/example/matchparfait/utils/Helpers.kt
+++ app/src/main/java/com/example/matchparfait/utils/Helpers.kt
@@ -1,5 +1,6 @@
 package com.example.matchparfait.utils
 
+import com.example.matchparfait.model.entitys.AddressUser
 import com.example.matchparfait.model.entitys.Product
 import com.example.matchparfait.model.entitys.User
 
@@ -10,6 +11,8 @@ class Helpers {
         private lateinit var token: String
         private lateinit var products : List<Product>
         private lateinit var selectProduct: Product
+        private lateinit var total : String
+        private lateinit var editAddress : AddressUser
 
         fun startSession(token : String, user : User){
             this.actualUser = user
@@ -29,59 +32,6 @@ class Helpers {
         }
 
         fun getProducts() : List<Product>{
-            /*val productList = listOf(
-                Product(
-                    productId = "1",
-                    productName = "Shampoo Anticaspa",
-                    productBrand = "Head & Shoulders",
-                    price = 50,
-                    quantity = 10,
-                    stars = 4,
-                    photo = "https://example.com/shampoo.jpg",
-                    description = "Shampoo anticaspa para todo tipo de cabello",
-                    ingredients = "Aqua, Sodium Laureth Sulfate, Zinc Carbonate",
-                    colors = "Blanco",
-                    photos = "https://example.com/shampoo1.jpg, https://example.com/shampoo2.jpg",
-                    type = "Cuidado del Cabello",
-                    notes = "Usar diariamente para mejores resultados",
-                    addition_date = "2023-01-15",
-                    classification = "Higiene"
-                ),
-                Product(
-                    productId = "2",
-                    productName = "Crema Hidratante",
-                    productBrand = "Nivea",
-                    price = 30,
-                    quantity = 20,
-                    stars = 5,
-                    photo = "https://example.com/crema.jpg",
-                    description = "Crema hidratante para piel seca",
-                    ingredients = "Aqua, Glycerin, Paraffinum Liquidum",
-                    colors = "Blanco",
-                    photos = "https://example.com/crema1.jpg, https://example.com/crema2.jpg",
-                    type = "Cuidado de la Piel",
-                    notes = "Aplicar por las mañanas y noches",
-                    addition_date = "2023-02-10",
-                    classification = "Higiene"
-                ),
-                Product(
-                    productId = "3",
-                    productName = "Pasta de Dientes",
-                    productBrand = "Colgate",
-                    price = 25,
-                    quantity = 15,
-                    stars = 4,
-                    photo = "https://lunaricode.com/assets/images/RBBL00001_1.png",
-                    description = "Pasta de dientes con flúor para protección contra caries",
-                    ingredients = "Sodium Fluoride, Aqua, Sorbitol",
-                    colors = "Blanco",
-                    photos = "https://example.com/pasta1.jpg, https://example.com/pasta2.jpg",
-                    type = "Cuidado Dental",
-                    notes = "Cepillar al menos dos veces al día",
-                    addition_date = "2023-03-05",
-                    classification = "0"
-                )
-            )*/
             return this.products
         }
 
@@ -91,6 +41,22 @@ class Helpers {
 
         fun getSelectedProduct() : Product{
             return this.selectProduct
+        }
+
+        fun saveTotal(amount : String){
+            this.total = amount
+        }
+
+        fun getTotal() : String{
+            return this.total
+        }
+
+        fun saveAddressToEdit(address : AddressUser){
+            this.editAddress = address
+        }
+
+        fun getAddressToEdit() : AddressUser{
+            return this.editAddress
         }
     }
 }
