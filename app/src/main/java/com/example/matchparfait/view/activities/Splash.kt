06b506c2abf51e3a63256e0d43a481f31e6af8d4
Andrea Rios <arios@fc5.mx>
2024-05-26 22:05:14 -0700

    Feature: se verifican preferencias del usuario en splash

--- app/src/main/java/com/example/matchparfait/view/activities/Splash.kt
+++ app/src/main/java/com/example/matchparfait/view/activities/Splash.kt
@@ -13,6 +13,9 @@ import com.example.matchparfait.presenter.ProductsPresenterImpl
 import com.example.matchparfait.presenter.interfaces.LoginPresenter
 import com.example.matchparfait.presenter.interfaces.ProductsPresenter
 import com.example.matchparfait.utils.Helpers
+import com.example.matchparfait.utils.Preferences
+import com.example.matchparfait.view.components.AlertDialog
+import com.example.matchparfait.view.components.Loading
 import com.example.matchparfait.view.interfaces.LoginView
 import com.example.matchparfait.view.interfaces.ProductsView
 
@@ -20,32 +23,52 @@ class Splash : AppCompatActivity(), LoginView, ProductsView{
 
     private var presenter : LoginPresenter = LoginPresenterImpl(this, this)
     private var preserP : ProductsPresenter = ProductsPresenterImpl(this, this)
+    private var preferences = Preferences<String>(this)
+    private lateinit var loading : Loading
+    private lateinit var alertDialog : AlertDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        this.presenter.Login("manuelvillegas820@gmail.com", "Contra123")
+
+        this.alertDialog = AlertDialog(this)
+        this.loading = Loading(this)
+
+        if(preferences.GetPreferenceSync("mail") == "NOT_FOUND"){
+            var intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        } else {
+            var mail = preferences.GetPreferenceSync("mail")
+            var passwod = preferences.GetPreferenceSync("password")
+            this.loading.show()
+            this.presenter.Login(mail, passwod)
+        }
     }
 
     override fun OnLoginSuccces(user: User) {
-        Toast.makeText(this, user.name, Toast.LENGTH_SHORT).show()
-        Log.d("LOGIN", "todo ok ;P")
         this.preserP.GetProducts()
     }
 
     override fun OnProductsGetted(products: List<Product>) {
-        Log.d("PROD", "todo ok ;P")
+        this.loading.dismiss()
         Helpers.saveProducts(products)
         var intent = Intent(this, Main::class.java)
         startActivity(intent)
     }
 
     override fun OnErrorGettingProducts(message: String) {
-        Log.d("ERROR PROF", message)
+        this.loading.show()
+        this.alertDialog.setMessage(message)
+        this.alertDialog.setImage(R.drawable.ic_star_worry)
+        this.alertDialog.show()
+        Log.d("ERROR GET PRODUCTS", message)
     }
 
     override fun OnLoginError(message: String) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        this.loading.dismiss()
+        this.alertDialog.setMessage(message)
+        this.alertDialog.setImage(R.drawable.ic_star_worry)
+        this.alertDialog.show()
         Log.d("ERROR LOGIN", message)
     }
 }
