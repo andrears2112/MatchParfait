a90d89a4588785e6236a01ff928d611f333ca3e9
Andrea Rios <arios@fc5.mx>
2024-05-27 06:00:57 -0700

    Feature: activity de registro

---
+++ app/src/main/java/com/example/matchparfait/view/activities/Register.kt
@@ -0,0 +1,14 @@
+package com.example.matchparfait.view.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.matchparfait.R
+
+class Register : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+
+}
