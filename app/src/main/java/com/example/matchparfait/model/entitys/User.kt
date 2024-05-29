500d434f32631c7e320bcff26fc04177564a4003
Andrea Rios <arios@fc5.mx>
2024-05-28 18:52:54 -0700

    Feature: se agregan variables a usuario

--- app/src/main/java/com/example/matchparfait/model/entitys/User.kt
+++ app/src/main/java/com/example/matchparfait/model/entitys/User.kt
@@ -10,6 +10,8 @@ data class User(
     var name : String = "",
     var lastName1 : String = "",
     var lastName2 : String = "",
+    var last_name1 : String = "",
+    var last_name2 : String = "",
     var phone_number : String = "",
     var password : String = "",
     var date_of_birth : String = "",
