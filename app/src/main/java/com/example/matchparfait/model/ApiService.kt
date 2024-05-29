4ba5df3ebb8b1b4eae20f4e40d228099b46aeed3
Andrea Rios <arios@fc5.mx>
2024-05-29 03:29:34 -0700

    Feature: se eliminan archivos en desuso

--- app/src/main/java/com/example/matchparfait/model/ApiService.kt
+++
@@ -1,9 +0,0 @@
-package com.example.matchparfait.model
-
-import retrofit2.Call
-import retrofit2.http.GET
-
-interface ApiService {
-    @GET("/api/v1/products")
-    fun getData() : Call<String>
-}
