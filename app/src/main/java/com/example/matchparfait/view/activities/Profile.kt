487a6a511a625d7f326afe9a70dda7bca5f91156
Andrea Rios <arios@fc5.mx>
2024-05-28 18:35:31 -0700

    Feature: activity de perfil

--- app/src/main/java/com/example/matchparfait/view/activities/Profile.kt
+++ app/src/main/java/com/example/matchparfait/view/activities/Profile.kt
@@ -1,12 +1,13 @@
-package com.example.matchparfait
+package com.example.matchparfait.view.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.matchparfait.R
 
-class UserProfile : AppCompatActivity() {
+class Profile : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.user)
+        setContentView(R.layout.activity_profile)
     }
 }
